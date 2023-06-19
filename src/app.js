08040c31b02d4f9bd4dde087486c90760eebfb34
breadworshipper <azhrayashna@gmail.com>
2023-06-19 15:09:14 +0700

    added article post endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
 
@@ -9,6 +10,8 @@ require("./model/article");
 
 const articleRoute = require("./routes/articleRoute"); // Access to articelRouter object in article.js
 app.use("/articles", articleRoute) // Set endpoint
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 var server = app.listen(3000, function(){
     console.log("Listening on port " + server.address().port);
--- src/routes/articleRoute.js
+++ src/routes/articleRoute.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router(); // Use router for individual router files
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 var Article = mongoose.model("Article");
 
-// TODO : GET all articles endpoint
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
 router.get("/", async function(req, res){
     var articles = await Article.find({}).exec();
     res.send(articles);
+});
+
+router.post("/", jsonParser, function(req, res){
+    const articleTitle = req.body.title;
+    const articleContent = req.body.content;
+
+    var newArticle = new Article({
+        title: articleTitle,
+        content: articleContent
+    });
+
+    newArticle.save();
+    res.send(newArticle);
 });
 
 module.exports = router; // Export module 
